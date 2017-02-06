9a151495ac4e6c77fae841e4844c65403cdc71ff
Puls <Puls@192.168.0.150>
2017-02-06 20:17:04 +0100

    change package

--- darksky-forecast-api-jackson/src/main/java/tk/plogitech/darksky/api/jackson/DarkSkyJacksonClient.java
+++ darksky-forecast-api-jackson/src/main/java/tk/plogitech/darksky/api/jackson/DarkSkyJacksonClient.java
@@ -1,27 +1,27 @@
 /*
- * The MIT License
- *
- * Copyright 2017 Philipp-André Plogmann.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+* The MIT License
+*
+* Copyright 2017 Philipp-André Plogmann.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
  */
-package de.pap.darksky.api.jackson;
+package tk.plogitech.darksky.api.jackson;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import static com.fasterxml.jackson.databind.MapperFeature.AUTO_DETECT_GETTERS;
@@ -32,16 +32,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import pap.darksky.forecast.APIKey;
-import pap.darksky.forecast.DarkSkyClient;
-import pap.darksky.forecast.ForecastException;
-import pap.darksky.forecast.ForecastRequest;
-import pap.darksky.forecast.ForecastRequestBuilder;
-import pap.darksky.forecast.GeoCoordinates;
-import pap.darksky.forecast.Latitude;
-import pap.darksky.forecast.Longitude;
-import pap.darksky.forecast.model.Forecast;
-import static pap.darkysky.forecast.util.Assert.nonNull;
+import tk.plogitech.darksky.forecast.APIKey;
+import tk.plogitech.darksky.forecast.DarkSkyClient;
+import tk.plogitech.darksky.forecast.ForecastException;
+import tk.plogitech.darksky.forecast.ForecastRequest;
+import tk.plogitech.darksky.forecast.ForecastRequestBuilder;
+import tk.plogitech.darksky.forecast.GeoCoordinates;
+import tk.plogitech.darksky.forecast.Latitude;
+import tk.plogitech.darksky.forecast.Longitude;
+import tk.plogitech.darksky.forecast.model.Forecast;
+import static tk.plogitech.darksky.forecast.util.Assert.notNull;
 
 /**
  * Service for DarkSky weather forecast API which uses Jackson to parse the response into JavaBean objects.
@@ -51,7 +51,7 @@ import static pap.darkysky.forecast.util.Assert.nonNull;
 public class DarkSkyJacksonClient extends DarkSkyClient {
 
     private static final Logger logger = Logger.getLogger(DarkSkyJacksonClient.class.getSimpleName());
-    private final ObjectMapper mapper = objectMapper();
+    private static final ObjectMapper mapper = objectMapper();
 
     /**
      * Returns the forecast as {@link Forecast} object parsed by Jackson.
@@ -62,11 +62,10 @@ public class DarkSkyJacksonClient extends DarkSkyClient {
      * @throws ForecastException if the forecast cannot be fetched.
      */
     public Forecast forecast(ForecastRequest request) throws ForecastException {
-        nonNull("The ForecastRequest cannot be null.", request);
+        notNull("The ForecastRequest cannot be null.", request);
+        logger.log(Level.FINE, "Executing Forecat request: {0}", request);
 
-        logger.log(Level.INFO, "Executing Forecat request: {0}", request);
-
-        try (InputStream is = executeRawForecastRequest(request.getUrl())) {
+        try (InputStream is = executeForecastRequest(request)) {
             return mapper.readValue(is, Forecast.class);
 
         } catch (IOException e) {
